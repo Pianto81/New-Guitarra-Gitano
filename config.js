f5cafe7370def94ac6b5fe85d161666fbffd5f06
Pianto81 <90965863+Pianto81@users.noreply.github.com>
2021-11-05 18:22:10 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    app: {
+        px: '&',
+        token: process.env.token,
+        playing: process.env.activity
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 1000,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
